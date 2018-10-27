01e24a37fda3548aad8cfa6634f4a2e5fb6ce218
Chris You <chrisyoufacebook@gmail.com>
2018-10-27 16:56:08 -0400

    Beautified Code

--- begin.php
+++ begin.php
@@ -1,15 +1,17 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	session_start();
-	$_SESSION['user_id'] = 1;
-	try{
-		$db = new PDO("mysql:host=$servername;dbname=todo", $username, $password);
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		//echo "Connected", '<br>';
-	}
-	catch(PDOException $e){
-		//echo "Can Not Connect". $e->getMessage();
-	}
+$servername = "localhost";
+$username = "root";
+$password = "";
+session_start();
+$_SESSION['user_id'] = 1;
+try {
+    $db = new PDO("mysql:host=$servername;dbname=todo", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected", '<br>';
+    
+}
+catch(PDOException $e) {
+    //echo "Can Not Connect". $e->getMessage();
+    
+}
 ?>
